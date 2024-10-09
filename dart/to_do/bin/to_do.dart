7044b60b8592e25b269848defcc04a6143859efb
Nawinn <stvgutzeit@gmail.com>
2024-10-09 02:28:16 +0200

    Workload

--- dart/to_do/bin/to_do.dart
+++ dart/to_do/bin/to_do.dart
@@ -1,13 +1,6 @@
 import 'package:to_do/to_do.dart' as todo;
 
-void main() async {
-  todo.Task task1 = todo.Task(1, 'Spülen', 'Spülen des Toiletten', false, false, DateTime.now());
-  todo.Task task2 = todo.Task(2, 'Putzen', 'Putzen des Hauses', false, false, DateTime.now());
-  todo.Task task3 = todo.Task(3, 'Kochen', 'Kochen der Suppe', false, false, DateTime.now());
-  todo.tasks.add(task1);
-  todo.tasks.add(task2);
-  todo.tasks.add(task3);
-  
+void main() {
   todo.loadTasks();
   todo.mainMenue();
   todo.saveTasks(); 
--- dart/to_do/lib/to_do.dart
+++ dart/to_do/lib/to_do.dart
@@ -19,13 +19,18 @@ class Task {
       'id': id,
       'title': title,
       'completed': completed,
-      'dueDate': dueDate,
+      'dueDate': dueDate.toIso8601String(),
     };
   }
 
   static Task fromJson(Map<String, dynamic> json) {
-    return Task(json['id'], json['title'], json['description'],
-        json['priority'], json['completed'], json['dueDate']);
+    return Task(json['id'], json['title'] ?? 'Unbekannt', json['description'] ?? 'Beschreibung',
+        json['priority'] ?? false, json['completed'] ?? false, DateTime.parse(json['dueDate'] ?? '0000-00-00'));
+  }
+
+  @override
+  String toString() {
+    return 'Task(id: $id, title: $title, description: $description, priority: $priority, completed: $completed, dueDate: $dueDate)';
   }
 }
 
@@ -42,6 +47,7 @@ void mainMenue() async {
     print('3. Aufgaben anzeigen');
     print('4. Aufgaben als erledigt markieren');
     print('5. Programm beenden');
+    print('6. Alle Aufgaben löschen');
     print('--------------------------');
     stdout.write('Auswahl: ');
     String? input = stdin.readLineSync();
@@ -64,6 +70,9 @@ void mainMenue() async {
         running = false;
         print('Programm beendet!');
         break;
+      case '6':
+        deleteAlltasks();
+        break;
       default:
         print('Ungültige Auswahl!');
     }
@@ -71,21 +80,27 @@ void mainMenue() async {
 }
 
 void addTask() {
-try {
+  try {
     stdout.write('Aufgabe eingeben: ');
     String? input = stdin.readLineSync();
+    String? description;
+    DateTime? dueDate;
     do {
       if (input == null || input.isEmpty) {
         print('Ungültige Eingabe!');
         print('--------------------------');
         return;
       } else {
-        stdout.write('Beschreibung eingeben: ');
-        String? description = stdin.readLineSync();
-        if (description == null || description.isEmpty) {
+        try {
+          stdout.write('Beschreibung eingeben: ');
+          description = stdin.readLineSync();
+          if (description == null || description.isEmpty) {
           description = 'Beschreibung';
-        } else {
+          } else { 
           print('$description hinzugefügt');
+          } 
+        } catch (ex) {
+          print('Fehler beim Eingeben der Beschreibung: $ex');
         }
         DateTime? dueDate;
         while (true) {
@@ -93,44 +108,54 @@ try {
           String? dateInput = stdin.readLineSync();
           if (dateInput == 'quit') {
             break;
-          }
-          try {
-            dueDate = DateTime.parse(dateInput!);
+          } else if (dateInput == null || dateInput.isEmpty) {
+            dueDate = DateTime.parse('0000-00-00');
             break;
-          } catch (ex) {
-            print(
-                '$ex \n\nFalsche Datumseingabe. Bitte erneut versuchen oder "quit" eingeben zum Verlassen');
+          } else if (RegExp(r'^\d{4}-\d{2}-\d{2}$').hasMatch(dateInput)) {
+            try {
+              dueDate = DateTime.parse(dateInput);
+              break;
+            } catch (e) {
+              print('Ungültiges Datumsformat. Bitte versuchen Sie es erneut.');
+            }
+          } else {
+            print('Ungültiges Datumsformat. Bitte verwenden Sie YYYY-MM-DD.');
           }
         }
-        tasks.add(
-            Task(tasks.length + 1, input, description, false, false, dueDate!));
+        tasks.add(Task(tasks.length + 1, input, description!, false, false, dueDate!));
         print('Aufgabe $input hinzugefügt!');
         print('--------------------------');
       }
     } while (input.isEmpty);
   } catch (ex) {
     print('Fehler beim Eingeben der Aufgabe: $ex');
-  }
+    }
 }
 
 
+void deleteAlltasks() {
+  tasks.clear();
+  print('Alle Aufgaben gelöscht!');
+  print('--------------------------');
+}
+
 void deleteTask() {
-  stdout.write('Zu löschende Aufgabe eingeben: ');
-  String? input = stdin.readLineSync();
-  if (input == null || input.isEmpty) {
-    print('Ungültige Eingabe!');
-    print('--------------------------');
-    return;
-  }
-  int index = tasks.indexWhere((task) => task.title == input);
-  if (index != -1) {
-    tasks.removeAt(index);
-    print('Aufgabe $input gelöscht!');
-    print('--------------------------');
-  } else {
-    print('Aufgabe nicht gefunden!');
-    print('--------------------------');
-  }
+  String? input;
+  showTasks();
+  print('--------------------------');
+  
+  stdout.write('Zu löschende Aufgaben ID eingeben: ');
+  do {
+    input = stdin.readLineSync();
+    try {
+      int index = int.parse(input!);
+      tasks.removeWhere((task) => task.id == index);
+      print('Aufgabe mit ID $index gelöscht!');
+      print('--------------------------');
+    } catch (ex) {
+      print('Fehler beim Löschen der Aufgabe: $ex');
+    }
+  } while (input == null || input.isEmpty);
 }
 
 void showTasks() {
@@ -147,33 +172,35 @@ void showTasks() {
 }
 
 void checkTask() {
-  stdout.write('Aufgabe eingeben: ');
-  String? input = stdin.readLineSync();
-  if (input == null || input.isEmpty) {
-    print('Ungültige Eingabe!');
-    print('--------------------------');
-    return;
-  }
-  bool found = false;
-  for (var task in tasks) {
-    if (task.title == input) {
-      task.completed = true;
-      print('Aufgabe $input als erledigt markiert!');
-      found = true;
-      break;
-    }
-  }
-  if (!found) {
-    print('Aufgabe nicht gefunden!');
-  }
+  String? input;
+
+  showTasks();
   print('--------------------------');
+
+  do {
+    stdout.write('Zu erledigende Aufgabe eingeben: ');
+    input = stdin.readLineSync();
+    if (input == null || input.isEmpty) {
+      print('Ungültige Eingabe!');
+      print('--------------------------');
+    } else {
+      try {
+        int taskId = int.parse(input); // Umwandlung in int
+        tasks.firstWhere((task) => task.id == taskId).completed = true;
+        print(tasks.firstWhere((task) => task.id == taskId).completed);
+        print('Aufgabe $taskId als erledigt markiert!');
+        print('--------------------------');
+      } catch (ex) {
+        print('Aufgabe mit ID $input nicht gefunden. \n\n Fehler: $ex');
+      }
+    }
+  } while (input == null || input.isEmpty);
 }
 
 void saveTasks() {
   try {
-    final File file_tasks = File('files/tasks.json');
-    file_tasks.writeAsStringSync(
-        jsonEncode(tasks.map((task) => task.toJson()).toList()));
+    final File fileTasks = File('files/tasks.json');
+    fileTasks.writeAsStringSync(jsonEncode(tasks.map((task) => task.toJson()).toList()));
   } catch (ex) {
     print('Fehler beim Speichern der Aufgaben: $ex');
   }
@@ -183,10 +210,30 @@ void loadTasks() async {
   print('Lade Aufgaben...');
   Duration duration = Duration(seconds: 2);
   await Future.delayed(duration);
-  final File file_tasks = File('files/tasks.json');
-  if (file_tasks.existsSync()) {
-    tasks = (jsonDecode(file_tasks.readAsStringSync()) as List)
-        .map((item) => Task.fromJson(item))
-        .toList();
+  final File fileTasks = File('files/tasks.json');
+  
+  print('Dateipfad: ${fileTasks.path}');
+  print('Datei existiert: ${fileTasks.existsSync()}');
+  
+  if (fileTasks.existsSync()) {
+    try {
+      String fileContent = fileTasks.readAsStringSync();
+      print('Dateiinhalt: $fileContent');
+      
+      final List<dynamic> jsonData = jsonDecode(fileContent);
+      print('JSON-Daten: $jsonData');
+      
+      tasks = jsonData.map((item) => Task.fromJson(item)).toList();
+      print('Geladene Aufgaben: $tasks');
+      
+      print('Aufgaben geladen!');
+      print('--------------------------');
+    } catch (ex) {
+      print('Fehler beim Laden der Aufgaben: $ex');
+      print('Stacktrace: ${StackTrace.current}');
+    }
+  } else {
+    print('Die Datei tasks.json existiert nicht.');
+    print('Aktuelles Verzeichnis: ${Directory.current.path}');
   }
 }
